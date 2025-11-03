ffb2577227a0f4bbd261dd8001b880b8c966df9c
LogWat <Nk8877wpt+Karan@gmail.com>
2025-11-04 01:29:25 +0900

    feat: 障害物融合コンポーネントとTebプランナーコンポーネントを追加

---
+++ d2_teb_local_planner/src/component/obs_fusion_component.cpp
@@ -0,0 +1,4 @@
+#include <rclcpp_components/register_node_macro.hpp>
+#include <d2_teb_local_planner/component/obs_fusion.hpp>
+
+RCLCPP_COMPONENTS_REGISTER_NODE(d2_teb_local_planner::ObstacleFusionComponent)
---
+++ d2_teb_local_planner/src/component/teb_planner_component.cpp
@@ -0,0 +1,4 @@
+#include <rclcpp_components/register_node_macro.hpp>
+#include <d2_teb_local_planner/component/teb_planner.hpp>
+
+RCLCPP_COMPONENTS_REGISTER_NODE(d2_teb_local_planner::TebMotionStandaloneComponent)
